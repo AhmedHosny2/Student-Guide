e1082f3570c8734dc4cf2365349b957adba369a3
Abdelrahman Elkhateeb <elkhateebabdelrahman@gmail.com>
2023-10-04 19:48:47 +0300

    updating navbar

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,12 +4,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#3490dc", // Replace with your primary color
-        secondary: {
-          100: "#e2e2d5",
-          200: "#888883",
-        },
-        main: "#e3342f",
+        primary: "#04364A",
+        secondary: "#176B87",
+        third: "#64CCC5",
       },
     },
   },
