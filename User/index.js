3c90a6b98609a91a2c74312a2b1307604db92c63
Ahmed Yehia <Ahmed.Hosny4434@gmail.com>
2023-10-09 00:12:34 +0300

    Adding continue with google

--- User/index.js
+++ User/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);
 const cors = require("cors");
 var cookies = require("cookie-parser");
+const passport = require("passport");
 require("dotenv").config();
 
 const db = require("./config/database.js");
@@ -17,10 +17,72 @@ app.use(
     origin: "http://127.0.0.1:5500",
   })
 );
+app.use(session({
+  resave: false,
+  saveUninitialized: true,
+  secret: process.env.SESSION_SECRET, 
+}));
 
 const MONGODB_URI = process.env.CONNECTION_URL;
 app.use(cookies());
 
+
+// habd  ============================
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/success', (req, res) => {
+  res.render('pages/success', {user: userProfile});
+});
+app.get('/error', (req, res) => res.send("error logging in"));
+ 
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+ 
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:5001/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+      userProfile=profile;
+      return done(null, userProfile);
+  }
+));
+ 
+app.get('/auth/google', 
+  passport.authenticate('google', { scope : ['profile', 'email'] }));
+ 
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/error' }),
+  function(req, res) {
+    const rawJSON = JSON.parse(userProfile._raw);
+
+    // Extract the information
+    const firstName = rawJSON.given_name;
+    const lastName = rawJSON.family_name;
+    const email = rawJSON.email;
+    
+    // You can use these values to store and verify the user later
+    console.log(`First Name: ${firstName}`);
+    console.log(`Last Name: ${lastName}`);
+    console.log(`Email: ${email}`);
+    
+    
+    res.redirect('http://127.0.0.1:5500/client/html/home.html');
+  });
+
+
+
+//=======================================================
 //routes 
 app.use("/user", router);
 
--- User/routes/user.js
+++ User/routes/user.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const { loginUser, signupUser } = require("../controller/user");
 const router = express.Router();
-const verifyToken = require("../middleware/auth").verifyToken;
-const verifyRole = require("../middleware/auth").verifyRole;
+const {verifyToken,verifyRole} = require("../middleware/auth");
+
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
+
 // for testing =============================================================
 router.get("/protected-route", verifyToken, (req, res, next) => {
+  console.log("wooow you made it to the protected route");
   res.send("You made it to the route.");
 });
-router.get("/admin-route", verifyRole, (req, res,next) => {
+router.get("/admin-route", verifyRole, (req, res, next) => {
   /*if checkToken function succeed, api reach this block
    */
   res.send("You made it to the route.");
