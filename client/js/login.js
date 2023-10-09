042f1dd557b096f63b71bb2dcc6e75f08640e3a2
Abdelrahman Elkhateeb <elkhateebabdelrahman@gmail.com>
2023-10-09 21:40:01 +0300

    styling login

--- client/js/login.js
+++ client/js/login.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const loginForm = document.getElementById("login-form");
-  const protectedRouteButton = document.getElementById("protected-route-button");
+  const loginForm = document.querySelector(".login-form");
+  const protectedRouteButton = document.getElementById(
+    "protected-route-button"
+  );
   const adminRouteButton = document.getElementById("admin-route-button");
 
   loginForm.addEventListener("submit", (e) => {
@@ -37,52 +39,64 @@ document.addEventListener("DOMContentLoaded", () => {
       });
   });
 
-  protectedRouteButton.addEventListener("click", () => {
-    const protectedApiUrl = "http://localhost:5001/user/protected-route"; // Replace with your protected API URL
+  // protectedRouteButton.addEventListener("click", () => {
+  //   const protectedApiUrl = "http://localhost:5001/user/protected-route"; // Replace with your protected API URL
 
-    fetch(protectedApiUrl, {
-      method: "GET",
-      credentials: "include",
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.text();
-      })
-      .then((data) => {
-        console.log("Response from Protected API Route:", data);
-        // Handle the response data as needed
-      })
-      .catch((error) => {
-        console.error("Fetch error:", error);
-      });
-  });
+  //   fetch(protectedApiUrl, {
+  //     method: "GET",
+  //     credentials: "include",
+  //   })
+  //     .then((response) => {
+  //       if (!response.ok) {
+  //         throw new Error("Network response was not ok");
+  //       }
+  //       return response.text();
+  //     })
+  //     .then((data) => {
+  //       console.log("Response from Protected API Route:", data);
+  //       // Handle the response data as needed
+  //     })
+  //     .catch((error) => {
+  //       console.error("Fetch error:", error);
+  //     });
+  // });
 
-  adminRouteButton.addEventListener("click", () => {
-    const adminApiUrl = "http://localhost:5001/user/admin-route"; // Replace with your admin API URL
+  // adminRouteButton.addEventListener("click", () => {
+  //   const adminApiUrl = "http://localhost:5001/user/admin-route"; // Replace with your admin API URL
 
-    fetch(adminApiUrl, {
-      method: "GET",
-      credentials: "include",
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.text();
-      })
-      .then((data) => {
-        console.log("Response from Admin API Route:", data);
-        // Handle the response data as needed
-      })
-      .catch((error) => {
-        console.error("Fetch error:", error);
-      });
-  });
-  document.getElementById("google-login-button").addEventListener("click", function() {
-    window.location.href = "http://localhost:5001/auth/google";
+  //   fetch(adminApiUrl, {
+  //     method: "GET",
+  //     credentials: "include",
+  //   })
+  //     .then((response) => {
+  //       if (!response.ok) {
+  //         throw new Error("Network response was not ok");
+  //       }
+  //       return response.text();
+  //     })
+  //     .then((data) => {
+  //       console.log("Response from Admin API Route:", data);
+  //       // Handle the response data as needed
+  //     })
+  //     .catch((error) => {
+  //       console.error("Fetch error:", error);
+  //     });
+  // });
+  document
+    .getElementById("google-login-button")
+    .addEventListener("click", function () {
+      window.location.href = "http://localhost:5001/auth/google";
+    });
+});
+//start login styling animation
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const wrapper = document.querySelector(".wrapper");
+
+signUpButton.addEventListener("click", () => {
+  wrapper.classList.add("right-panel-active");
 });
 
-
+signInButton.addEventListener("click", () => {
+  wrapper.classList.remove("right-panel-active");
 });
