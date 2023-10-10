b444358d21645bee389fd4b059f29786b995d535
Ahmed Yehia <Ahmed.Hosny4434@gmail.com>
2023-10-10 10:16:55 +0300

    course Selection page

--- client/js/courseSelection.js
+++ client/js/courseSelection.js
@@ -1,53 +1,38 @@
-const semesterInput = document.getElementById('semester');
-const courseSelect = document.getElementById('course');
-const contentDisplay = document.getElementById('content');
-const courseForm = document.getElementById('course-form');
-
-// Define a dictionary of course names for each semester
-const courseData = {
-    1: ['Math 1', 'Physics', 'Techn', 'CS 1'],
-    2: ['Math 2', 'OOP', 'Network', 'Micro', 'Theo'],
-    3: ['CPP', 'OS', 'DSA', 'DB', 'Math 3'],
-    4: ['SE', 'DS', 'IT', 'DIS', 'Media', 'Math 4'],
+// Define the courses for each semester
+const courses = {
+    1: ["Math 1", "Physics", "Techn", "CS 1"],
+    2: ["Math 2", "OOP", "Network", "Micro", "Theo"],
+    3: ["CPP", "OS", "DSA", "DB", "Math 3"],
+    4: ["SE", "DS", "IT", "DIS", "Media", "Math 4"]
 };
 
-// Populate the course dropdown based on the selected semester
-semesterInput.addEventListener('change', () => {
-    const selectedSemester = semesterInput.value;
-    const courses = courseData[selectedSemester] || [];
+// Get the select element
+const semesterSelect = document.getElementById("semester");
+const courseList = document.getElementById("course-list");
 
-    // Clear previous options
-    courseSelect.innerHTML = '<option value="" disabled selected>Select a Course</option>';
+// Function to display courses for the selected semester
+function displayCourses() {
+    const selectedSemester = semesterSelect.value;
+    const selectedCourses = courses[selectedSemester];
 
-    // Add new options
-    courses.forEach((courseName) => {
-        const option = document.createElement('option');
-        option.value = courseName;
-        option.textContent = courseName;
-        courseSelect.appendChild(option);
-    });
-});
+    // Clear the existing course list
+    courseList.innerHTML = "";
 
-// Handle form submission
-courseForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const selectedSemester = semesterInput.value;
-    const selectedCourse = courseSelect.value;
-
-    if (selectedSemester && selectedCourse) {
-        // Construct the URL with the course name as a query parameter
-        const url = `/get-content?course=${encodeURIComponent(selectedCourse)}`;
-console.log(url);
-        // Send a GET request to the backend using the constructed URL
-        fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-            // Display the received content in the paragraph
-            contentDisplay.textContent = data.content;
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-            contentDisplay.textContent = 'An error occurred while fetching content.';
+    // Display the courses for the selected semester as buttons
+    if (selectedCourses) {
+        selectedCourses.forEach(course => {
+            const button = document.createElement("button");
+            button.className = "course-button";
+            button.textContent = course;
+            courseList.appendChild(button);
         });
+    } else {
+        courseList.textContent = "No courses available for this semester.";
     }
-});
+}
+
+// Initially display courses for the selected semester
+displayCourses();
+
+// Add an event listener to update the course list when the user selects a different semester
+semesterSelect.addEventListener("change", displayCourses);
