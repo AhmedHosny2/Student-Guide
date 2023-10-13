e3d0373c80c2366ae66180f2474dc981a7909790
Abdelrahman Elkhateeb <elkhateebabdelrahman@gmail.com>
2023-10-13 18:30:50 +0300

    styling create course page

--- client/js/createCourse.js
+++ client/js/createCourse.js
@@ -33,9 +33,28 @@ document.addEventListener("DOMContentLoaded", function () {
         .catch((error) => {
           console.error("Fetch error:", error);
         });
-
     } catch (err) {
       console.error(err);
     }
+  });
+});
+//add text area api
+document.addEventListener("DOMContentLoaded", function () {
+  tinymce.init({
+    selector: "textarea",
+    plugins:
+      "ai tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss",
+    toolbar:
+      "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
+    tinycomments_mode: "embedded",
+    tinycomments_author: "Author name",
+    mergetags_list: [
+      { value: "First.Name", title: "First Name" },
+      { value: "Email", title: "Email" },
+    ],
+    ai_request: (request, respondWith) =>
+      respondWith.string(() =>
+        Promise.reject("See docs to implement AI Assistant")
+      ),
   });
 });
