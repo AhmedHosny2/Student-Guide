ccee802542442397a603417f2953603f62415395
Ahmed Yehia <Ahmed.Hosny4434@gmail.com>
2023-10-13 20:31:41 +0300

    Update BaseURLs.js

--- users/services/BaseURLs.js
+++ users/services/BaseURLs.js
@@ -1,4 +1,3 @@
 module.exports = {
-    BASE_URL: 'http://localhost:5001',
-    USER_BASE_URL: 'https://student-guide-users.vercel.app',
+    BASE_URL: 'https://student-guide-users.vercel.app',
   };
