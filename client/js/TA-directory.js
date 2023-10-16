8dabeeca212a737031ae03fed3f72b51822060c8
Abdelrahman Elkhateeb <elkhateebabdelrahman@gmail.com>
2023-10-17 01:49:36 +0300

    adding search bar to ta directory

--- client/js/TA-directory.js
+++ client/js/TA-directory.js
@@ -1,1 +1,16 @@
 // start search bar
+const arrOfSubjects = [];
+const cards = document.querySelectorAll(".card");
+const cardsNames = document.querySelectorAll(".card .course");
+cardsNames.forEach((ele) => {
+  arrOfSubjects.push(ele.outerText);
+});
+
+const searchBar = document.getElementById("searchBar");
+searchBar.addEventListener("input", (e) => {
+  const value = e.target.value.toLowerCase();
+  arrOfSubjects.forEach((e, i) => {
+    const isVisible = e.toLowerCase().includes(value);
+    cards[i].classList.toggle("hide", !isVisible);
+  });
+});
