9dd2dcf11f5af44942217ca79d5b906ecd19622c
Ahmed Yehia <Ahmed.Hosny4434@gmail.com>
2023-10-16 12:39:37 +0300

    Update user.js

--- users/routes/user.js
+++ users/routes/user.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-const { loginUser, signupUser, getUser , logoutUser} = require("../controller/user");
+const {
+  loginUser,
+  signupUser,
+  getUser,
+  logoutUser,
+} = require("../controller/user");
 const {
   verifyToken,
   verifyRole,
   testVerifyRole,
   testVerifyToken,
 } = require("../middleware/auth");
-router.post("/logout",logoutUser)
+router.post("/logout", logoutUser);
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
 router.use(verifyToken);
 router.get("/protected-route", testVerifyToken);
 router.get("/admin-route", verifyRole, testVerifyRole);
 router.get("/:userId", getUser);
-
 
 module.exports = router;
