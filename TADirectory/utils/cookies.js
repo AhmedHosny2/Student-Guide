eb3a6251d6658c422c264573301d9a3a40bd80a2
Ahmed Yehia <Ahmed.Hosny4434@gmail.com>
2023-10-18 23:05:52 +0300

    fix cookie

--- TADirectory/utils/cookies.js
+++ TADirectory/utils/cookies.js
@@ -1,9 +1,11 @@
 const jwt = require('jsonwebtoken');
 const secret = process.env.ACCESS_TOKEN_SECRET;
 function getEntriesFromCookie(req) {
-  let authcookie=(decodeURIComponent(req.headers.cookie).split("=")[1].split(";")[0])
-  authcookie =JSON.parse(authcookie);
-  authcookie = authcookie.token;
+  console.log("auth cooki 12 \n\n\n"+req.headers.cookie);
+  let authcookie=(decodeURIComponent(req.headers.cookie).split("=")[1])
+  console.log("auth cooki 11 \n\n\n"+authcookie);
+  // authcookie =JSON.parse(authcookie);
+  // authcookie = authcookie.token;
 console.log("auth cooki \n\n\nn\n"+authcookie);
 
   const decoded = jwt.verify(authcookie, secret);
