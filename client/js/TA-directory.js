47674628f65feaedcac10515d5b36fdc9456f4b5
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2023-10-20 21:51:05 +0300

    Update TA-directory.js

--- client/js/TA-directory.js
+++ client/js/TA-directory.js
@@ -46,7 +46,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const searchValue = e.target.value.toLowerCase();
     const cards = document.querySelectorAll(".card");
 
-<<<<<<< HEAD
     cards.forEach((card) => {
       const courseName = card
         .querySelector(".course")
@@ -54,64 +53,5 @@ document.addEventListener("DOMContentLoaded", function () {
       const isVisible = courseName.includes(searchValue);
       card.classList.toggle("hide", !isVisible);
     });
-=======
-    const box = document.createElement("div");
-    box.classList.add("box");
-
-    // Helper function to create a card wrap element
-    function createCardWrap(label, value) {
-      const wrap = document.createElement("div");
-      wrap.classList.add("wrap");
-
-      const p = document.createElement("p");
-      p.textContent = label;
-
-      const span = document.createElement("span");
-      span.textContent = value;
-
-      wrap.appendChild(p);
-      wrap.appendChild(span);
-
-      return wrap;
-    }
-
-    box.appendChild(createCardWrap("name", ta.name));
-    box.appendChild(createCardWrap("office hour", ta.officeHours));
-    box.appendChild(createCardWrap("office location", ta.officeLocation));
-    box.appendChild(createCardWrap("tutorials", ta.tutorials));
-
-    // Create the email element with a clickable link
-    const emailWrap = createCardWrap("email", "");
-    const emailLink = document.createElement("a");
-    emailLink.href = `mailto:${ta.email}`;
-    emailLink.textContent = ta.email;
-    emailLink.style.color = "gray";
-    emailWrap.appendChild(emailLink);
-    box.appendChild(emailWrap);
-
-    card.appendChild(courseName);
-    card.appendChild(box);
-
-    cardContainer.appendChild(card);
-  });
-}
-
-// Call the function to generate and append the cards
-fetchTAData();
-
-// Add an input event listener to the search bar
-const card = document.querySelectorAll(".card");
-const searchBar = document.getElementById("searchBar");
-searchBar.addEventListener("input", (e) => {
-  const searchValue = e.target.value.toLowerCase();
-
-  // Loop through all the cards
-  card.forEach((card) => {
-    const courseName = card.querySelector(".course").textContent.toLowerCase();
-    const isVisible = courseName.includes(searchValue);
-    console.log(isVisible);
-    card.style.display = isVisible ? "block" : "none";
-    console.log(card);
->>>>>>> 57d6af05d37a580a8fe04fb44a1ec8e729ba47b3
   });
 });
