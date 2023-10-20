8a808fcc4389257c4ebfa92ddb3dc3d465c2dbd7
Ahmed Yehia <Ahmed.Hosny4434@gmail.com>
2023-10-20 17:34:40 +0300

    fix auto complete

--- client/js/TA-directory.js
+++ client/js/TA-directory.js
@@ -1,101 +1,96 @@
-// start search bar
-const arrOfSubjects = [];
-const cards = document.querySelectorAll(".card");
-const cardsNames = document.querySelectorAll(".card .course");
-cardsNames.forEach((ele) => {
-  arrOfSubjects.push(ele.outerText);
-});
-
-const searchBar = document.getElementById("searchBar");
-searchBar.addEventListener("input", (e) => {
-  const value = e.target.value.toLowerCase();
-  arrOfSubjects.forEach((e, i) => {
-    const isVisible = e.toLowerCase().includes(value);
-    cards[i].classList.toggle("hide", !isVisible);
-  });
-});
-
-// yaya
-function fetchTAData() {
-  const apiUrl = `https://ta.ahmed-yehia.me/TADirectory`; // Replace with your API URL here
-  fetch(apiUrl, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    credentials: "include",
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      generateTACards(data);
-      console.log("Response from the API:", data);
-      // Handle the response data from the API (e.g., show a success message)
-    })
-    .catch((error) => {
-      console.error("Fetch error:", error);
-    });
-}
-// Sample array of TA objects
-
-// Function to generate and append cards
-function generateTACards(taData) {
-  const cardContainer = document.querySelector(".card-wrap");
-
-  taData.forEach((ta) => {
-    const card = document.createElement("section");
-    card.classList.add("card");
-
-    const courseName = document.createElement("div");
-    courseName.classList.add("course");
-    courseName.textContent = ta.course; // Replace with the actual course name
-
-    const box = document.createElement("div");
-    box.classList.add("box");
-
-    // Helper function to create a card wrap element
-    function createCardWrap(label, value) {
-      const wrap = document.createElement("div");
-      wrap.classList.add("wrap");
-
-      const p = document.createElement("p");
-      p.textContent = label;
-
-      const span = document.createElement("span");
-      span.textContent = value;
-
-      wrap.appendChild(p);
-      wrap.appendChild(span);
-
-      return wrap;
-    }
-
-    box.appendChild(createCardWrap("name", ta.name));
-    box.appendChild(createCardWrap("office hour", ta.officeHours));
-    box.appendChild(createCardWrap("office location", ta.officeLocation));
-    box.appendChild(createCardWrap("tutorials", ta.tutorials));
-
-    // Create the email element with a clickable link
-    const emailWrap = createCardWrap("email", "");
-    const emailLink = document.createElement("a");
-    emailLink.href = `mailto:${ta.email}`;
-    emailLink.textContent = ta.email;
-    emailLink.style.color = "gray";
-    emailWrap.appendChild(emailLink);
-    box.appendChild(emailWrap);
-
-    card.appendChild(courseName);
-    card.appendChild(box);
-
-    cardContainer.appendChild(card);
-  });
-}
-
-// Call the function to generate and append the cards
 document.addEventListener("DOMContentLoaded", function () {
+  const searchBar = document.getElementById("searchBar");
+  const cards = document.querySelectorAll(".card");
+
+  // Add an input event listener to the search bar
+  searchBar.addEventListener("input", (e) => {
+    const searchValue = e.target.value.toLowerCase();
+
+    // Loop through all the cards
+    cards.forEach((card) => {
+      const courseName = card.querySelector(".course").textContent.toLowerCase();
+      const isVisible = courseName.includes(searchValue);
+      card.style.display = isVisible ? "block" : "none";
+    });
+  });
+
+  function fetchTAData() {
+    const apiUrl = `https://ta.ahmed-yehia.me/TADirectory`; // Replace with your API URL here
+    fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        generateTACards(data);
+        console.log("Response from the API:", data);
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
+      });
+  }
+
+  // Your existing code for generating cards
+  function generateTACards(taData) {
+    const cardContainer = document.querySelector(".card-wrap");
+
+    taData.forEach((ta) => {
+      const card = document.createElement("section");
+      card.classList.add("card");
+
+      const courseName = document.createElement("div");
+      courseName.classList.add("course");
+      courseName.textContent = ta.course; // Replace with the actual course name
+
+      const box = document.createElement("div");
+      box.classList.add("box");
+
+      // Helper function to create a card wrap element
+      function createCardWrap(label, value) {
+        const wrap = document.createElement("div");
+        wrap.classList.add("wrap");
+
+        const p = document.createElement("p");
+        p.textContent = label;
+
+        const span = document.createElement("span");
+        span.textContent = value;
+
+        wrap.appendChild(p);
+        wrap.appendChild(span);
+
+        return wrap;
+      }
+
+      box.appendChild(createCardWrap("name", ta.name));
+      box.appendChild(createCardWrap("office hour", ta.officeHours));
+      box.appendChild(createCardWrap("office location", ta.officeLocation));
+      box.appendChild(createCardWrap("tutorials", ta.tutorials));
+
+      // Create the email element with a clickable link
+      const emailWrap = createCardWrap("email", "");
+      const emailLink = document.createElement("a");
+      emailLink.href = `mailto:${ta.email}`;
+      emailLink.textContent = ta.email;
+      emailLink.style.color = "gray";
+      emailWrap.appendChild(emailLink);
+      box.appendChild(emailWrap);
+
+      card.appendChild(courseName);
+      card.appendChild(box);
+
+      cardContainer.appendChild(card);
+    });
+  }
+
+  // Call the function to generate and append the cards
   fetchTAData();
 });
