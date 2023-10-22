0ad5fda3905c440f8588940514c962a78bb3282c
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2023-10-22 13:51:28 +0300

    update navbar and style home page

--- client/js/home.js
+++ client/js/home.js
@@ -1,45 +1,70 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const loadingScreen = document.getElementById("loading-screen");
-  // Function to get the current user and add their name to local storage
-  const getCurrentUser = async () => {
-    const apiUrl = "https://student-guide-users.ahmed-yehia.me/user"; //here
-    try {
-      const response = await fetch(apiUrl, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-      });
-      const data = await response.json();
-      if (data.email) {
-        localStorage.setItem("userEmail", data.email);
-        localStorage.setItem("userName", data.userName);
-        localStorage.setItem("isAdmin", data.isAdmin);
-        // Check if the user is logged in and display a message
-        //create the user greeting
-        const userGreeting = document.querySelector(".greetingUser");
+// document.addEventListener("DOMContentLoaded", () => {
+//   const loadingScreen = document.getElementById("loading-screen");
+//   // Function to get the current user and add their name to local storage
+//   const getCurrentUser = async () => {
+//     const apiUrl = "https://student-guide-users.ahmed-yehia.me/user"; //here
+//     try {
+//       const response = await fetch(apiUrl, {
+//         method: "GET",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         credentials: "include",
+//       });
+//       const data = await response.json();
+//       if (data.email) {
+//         localStorage.setItem("userEmail", data.email);
+//         localStorage.setItem("userName", data.userName);
+//         localStorage.setItem("isAdmin", data.isAdmin);
+//         // Check if the user is logged in and display a message
+//         //create the user greeting
+//         const userGreeting = document.querySelector(".greetingUser");
 
-        if (userGreeting) {
-          const greeting = document.createElement("h1");
-          greeting.textContent = `Welcome ${data.userName}!`;
-          greeting.style.textAlign = "center";
-          userGreeting.appendChild(greeting);
+//         if (userGreeting) {
+//           const greeting = document.createElement("h1");
+//           greeting.textContent = `Welcome ${data.userName}!`;
+//           greeting.style.textAlign = "center";
+//           userGreeting.appendChild(greeting);
 
-          loadingScreen.style.display = "none";
-        } else {
-          console.log("Element with class 'greetingUser' not found.");
-        }
-      } else {
-        // Redirect to the login page if no user is logged in
-        window.location.href = "https://www.ahmed-yehia.me/html/login.html";
-      }
-    } catch (error) {
-      console.error("Fetch error:", error);
-      window.location.href = "https://www.ahmed-yehia.me/html/login.html";
-    }
-  };
+//           loadingScreen.style.display = "none";
+//         } else {
+//           console.log("Element with class 'greetingUser' not found.");
+//         }
+//       } else {
+//         // Redirect to the login page if no user is logged in
+//         window.location.href = "https://www.ahmed-yehia.me/html/login.html";
+//       }
+//     } catch (error) {
+//       console.error("Fetch error:", error);
+//       window.location.href = "https://www.ahmed-yehia.me/html/login.html";
+//     }
+//   };
 
-  // Call the getCurrentUser function to check if the user is logged in
-  getCurrentUser();
-});
+//   // Call the getCurrentUser function to check if the user is logged in
+//   getCurrentUser();
+// });
+
+// start sout animation
+const arrOfGreetings = [
+  "Hello World!",
+  "¡Hola Mundo!",
+  "Hallo Welt!",
+  "Ciao Mondo!",
+  "مرحبا بالعالم",
+];
+
+const greeting = document.getElementById("greeting");
+const h1 = document.createElement("h1");
+h1.innerHTML = "welcome";
+greeting.appendChild(h1);
+
+let currentIndex = 0;
+
+setInterval(() => {
+  h1.style.opacity = 0; // Set opacity to 0 to fade out
+  setTimeout(() => {
+    h1.innerHTML = arrOfGreetings[currentIndex];
+    h1.style.opacity = 1; // Set opacity to 1 to fade in
+    currentIndex = (currentIndex + 1) % arrOfGreetings.length;
+  }, 1000);
+}, 2000);
--- client/js/navbar.js
+++ client/js/navbar.js
@@ -3,8 +3,13 @@ const hamburgerMenu = document.querySelector(".burger-menu");
 const navBar = document.querySelector(".header .nav");
 
 hamburgerMenu.addEventListener("click", () => {
-  hamburgerMenu.classList.toggle("active");
-  navBar.classList.toggle("active");
+  const vis = navBar.getAttribute("data-visibility");
+  if (vis === "false") {
+    navBar.setAttribute("data-visibility", "true");
+    hamburgerMenu.classList.add("active");
+  } else {
+    navBar.setAttribute("data-visibility", "false");
+    hamburgerMenu.classList.remove("active");
+  }
 });
-
 
