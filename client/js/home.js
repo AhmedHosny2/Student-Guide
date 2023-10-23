58f2eba5fe51dc7c5f8d16fc5801212e16bc22c7
Ahmed Yehia <Ahmed.Hosny4434@gmail.com>
2023-10-23 15:29:23 +0300

    Update home.js

--- client/js/home.js
+++ client/js/home.js
@@ -1,3 +1,29 @@
+// start sout animation
+const arrOfGreetings = [
+  "Hello World!",
+  "¡Hola Mundo!",
+  "Hallo Welt!",
+  "Ciao Mondo!",
+  "!مرحبا بالعالم",
+];
+
+const greeting = document.getElementById("greeting");
+const h1 = document.createElement("h1");
+h1.innerHTML = "welcome";
+greeting.appendChild(h1);
+
+let currentIndex = 0;
+
+setInterval(() => {
+  h1.style.opacity = 0; // Set opacity to 0 to fade out
+  setTimeout(() => {
+    h1.innerHTML = arrOfGreetings[currentIndex];
+    h1.style.opacity = 1; // Set opacity to 1 to fade in
+    currentIndex = (currentIndex + 1) % arrOfGreetings.length;
+  }, 1000);
+}, 2000);
+
+// animate css transform
 document.addEventListener("DOMContentLoaded", () => {
   const loadingScreen = document.getElementById("loading-screen");
   // Function to get the current user and add their name to local storage
@@ -31,32 +57,5 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
-
+  if (localStorage.getItem("userName") == null) getCurrentUser();
 });
-  getCurrentUser();
-// start sout animation
-const arrOfGreetings = [
-  "Hello World!",
-  "¡Hola Mundo!",
-  "Hallo Welt!",
-  "Ciao Mondo!",
-  "!مرحبا بالعالم",
-];
-
-const greeting = document.getElementById("greeting");
-const h1 = document.createElement("h1");
-h1.innerHTML = "welcome";
-greeting.appendChild(h1);
-
-let currentIndex = 0;
-
-setInterval(() => {
-  h1.style.opacity = 0; // Set opacity to 0 to fade out
-  setTimeout(() => {
-    h1.innerHTML = arrOfGreetings[currentIndex];
-    h1.style.opacity = 1; // Set opacity to 1 to fade in
-    currentIndex = (currentIndex + 1) % arrOfGreetings.length;
-  }, 1000);
-}, 2000);
-
-// animate css transform
