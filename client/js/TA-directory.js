f4784a3c12e7d0cac24b06e8c2ead27ba39daeef
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2023-10-26 17:45:37 +0300

    update search bar

--- client/js/TA-directory.js
+++ client/js/TA-directory.js
@@ -1,12 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-  if (localStorage.getItem("userName") == null)
-  window.location.href = "https://www.ahmed-yehia.me/html/login.html";
-else {
-  const loginButton = document.querySelectorAll(".login");
-  loginButton.forEach((button) => {
-    button.style.display = "none";
-  });
-}
+// document.addEventListener("DOMContentLoaded", function () {
+//   if (localStorage.getItem("userName") == null)
+//     window.location.href = "https://www.ahmed-yehia.me/html/login.html";
+//   else {
+//     const loginButton = document.querySelectorAll(".login");
+//     loginButton.forEach((button) => {
+//       button.style.display = "none";
+//     });
+//   }
+
   let arr = [];
   function fetchTAData() {
     const apiUrl = `https://ta.ahmed-yehia.me/TADirectory`; // Replace with your API URL here
