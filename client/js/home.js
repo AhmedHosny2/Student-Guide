87d5b0abb6577c87adbe25194f4034bcfb035b29
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2023-10-27 14:02:58 +0200

    add animation to the home

--- client/js/home.js
+++ client/js/home.js
@@ -33,3 +33,35 @@ setInterval(() => {
     currentIndex = (currentIndex + 1) % arrOfGreetings.length;
   }, 1000);
 }, 2000);
+
+//start animation
+const sectionPoints = document.querySelectorAll(
+  ".section-global .section-points li"
+);
+const observerOfSectionPoints = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((el) => {
+      if (el.isIntersecting) el.target.classList.add("active");
+    });
+  },
+  { threshold: 1 }
+);
+sectionPoints.forEach((el) => observerOfSectionPoints.observe(el));
+
+
+
+const welcomeText = document.querySelectorAll(".welcome .container .text h1");
+const soutText = document.querySelectorAll(".welcome .container .sout");
+const logo = document.querySelectorAll(".welcome .container .img img");
+const observerOfWelcome = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((el) => {
+      if (el.isIntersecting) el.target.classList.add("active");
+    });
+  },
+  { threshold: 1 }
+);
+
+welcomeText.forEach((el) => observerOfWelcome.observe(el));
+soutText.forEach((el) => observerOfWelcome.observe(el));
+logo.forEach((el) => observerOfWelcome.observe(el));
