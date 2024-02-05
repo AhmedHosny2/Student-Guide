e62cba588d6f63cab15e87b417d415afdd6f1736
Ahmed Osama <98207790+AhmedHosny2@users.noreply.github.com>
2024-02-05 13:52:08 +0200

    update env for deployment

--- client/utils/env.js
+++ client/utils/env.js
@@ -1,13 +1,15 @@
 // for deployed version
-// export const coursesURL = `https://student-guide-course.ahmed-yehia.me/course`;
-// export const taURL = `https://ta.ahmed-yehia.me/TADirectory`;
-// export const userURL = "https://student-guide-users.ahmed-yehia.me/user";
-// export const clientLoginURL = "https://guide.ahmed-yehia.me/html/login.html";
-// export const clientURL = "https://guide.ahmed-yehia.me";
+
+
+export const coursesURL = `https://student-guide-course.ahmed-yehia.me/course`;
+export const taURL = `https://ta.ahmed-yehia.me/TADirectory`;
+export const userURL = "https://student-guide-users.ahmed-yehia.me/user";
+export const clientLoginURL = "https://guide.ahmed-yehia.me/html/login.html";
+export const clientURL = "https://guide.ahmed-yehia.me";
 
 // for local version
-export const userURL = "http://localhost:5001/user";
-export const apiUrl = `http://localhost:5003/TADirectory`;
-export const taURL = `http://localhost:5002/course`;
-export const clientURL = "http://127.0.0.1:5501/client/";
-export const clientLoginURL = "http://127.0.0.1:5501/client/html/login.html";
+// export const userURL = "http://localhost:5001/user";
+// export const apiUrl = `http://localhost:5003/TADirectory`;
+// export const taURL = `http://localhost:5002/course`;
+// export const clientURL = "http://127.0.0.1:5501/client/";
+// export const clientLoginURL = "http://127.0.0.1:5501/client/html/login.html";
