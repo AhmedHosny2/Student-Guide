9815731479acd47e90cfe205c166e1e3e1a5fdfc
Ahmed Yehia <98207790+AhmedHosny2@users.noreply.github.com>
2024-02-12 10:27:43 +0200

    adding smart in-memory cache

--- TADirectory/controller/TADirectory.js
+++ TADirectory/controller/TADirectory.js
@@ -1,49 +1,52 @@
 const { TaModel } = require("../model/TADirectory");
 const { TaCourseModel } = require("../model/TADirectory");
+let TAs = [];
+let TACourses = [];
 
 exports.getAllTas = async (req, res) => {
   try {
-      const tas = await TaModel.find().lean() 
-       return res.status(200).json(tas);
+    if (TAs.length > 0) {
+      return res.status(200).json(TAs);
+    }
+    const tas = await TaModel.find().lean();
+    TAs = tas;
+    return res.status(200).json(tas);
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
 exports.addTa = async (req, res) => {
   const { name, email, officeLocation } = req.body;
   const uniEmail = email + "@giu-uni.de";
-  // reset the cache
-  client.del("tas");
-  console.log(uniEmail);
+
   const found = await TaModel.find({ email: uniEmail });
   if (found !== null && found.length > 0) {
-    return res.status(200).json({ message: "TA already exists" });
+    return res.status(200).json({ message: "TAs already exists" });
   }
 
   try {
-    await TaModel.create({
+    const newTA = await TaModel.create({
       name,
       email: uniEmail,
       officeLocation,
     });
-    // updateUserPoints(userEmail, 10);
+    TAs.push(newTA);
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Internal server error" });
   }
-  res.status(200).json({ message: "TA created" });
+  res.status(200).json({ message: "TAs created" });
 };
 
-let oldTa = [];
 exports.getTaCourses = async (req, res) => {
   try {
-    console.log("get all tas");
-
-      if(oldTa.length > 0) 
-      return res.status(200).json(oldTa);
-     const tas = await TaCourseModel.find();
-     oldTa = tas;
+    if (TACourses.length > 0) {
+      return res.status(200).json(TACourses);
+    }
+    const tas = await TaCourseModel.find().lean();
+    TACourses = tas;
     res.status(200).json(tas);
   } catch (err) {
     console.error(err);
@@ -56,13 +59,13 @@ exports.assignTa = async (req, res) => {
   const { email, tutorials, courseName, officeHours } = req.body;
   let found = await TaModel.find({ email: email });
   if (found === null || found.length === 0) {
-    return res.status(404).json({ message: "TA not found" });
+    return res.status(404).json({ message: "TAs not found" });
   }
   found = found[0];
   try {
     console.log(found);
 
-    await TaCourseModel.create({
+    const newTAAssign = await TaCourseModel.create({
       name: found.name,
       officeLocation: found.officeLocation,
       email,
@@ -70,22 +73,8 @@ exports.assignTa = async (req, res) => {
       courseName,
       officeHours,
     });
-    res.status(200).json({ message: "TA assigned" });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
-exports.deleteTa = async (req, res) => {
-  const { email } = req.body;
-  // reset cache
-  client.del("tas");
-  try {
-    await TaModel.deleteOne({ email });
-    res.status(200).json({ message: "TA deleted" });
-    await TaCourseModel.deleteMany({
-      email,
-    });
+    TACourses.push(newTAAssign);
+    res.status(200).json({ message: "TAs assigned" });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Internal server error" });
@@ -93,12 +82,34 @@ exports.deleteTa = async (req, res) => {
 };
 exports.deleteTaCourse = async (req, res) => {
   const { _id } = req.body;
-  // reset cache
-  client.del("taCourses");
-
+  console.log(req.body);
   try {
     await TaCourseModel.deleteOne({ _id });
-    res.status(200).json({ message: "TA course deleted" });
+    const indexToDelete = TACourses.findIndex((myTA) => myTA._id.valueOf() === _id);
+    console.log(indexToDelete);
+    if (indexToDelete !== -1) {
+      TACourses.splice(indexToDelete, 1);
+    }
+    console.log(TACourses);
+    res.status(200).json({ message: "TAs course deleted" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+exports.deleteTa = async (req, res) => {
+  const { email } = req.body;
+  try {
+    await TaModel.deleteOne({ email });
+    const indexToDelete = TA.findIndex((myTA) => myTA.email === email);
+    if (indexToDelete !== -1) {
+      TA.splice(indexToDelete, 1);
+    }
+    res.status(200).json({ message: "TAs deleted" });
+    await TaCourseModel.deleteMany({
+      email,
+    });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Internal server error" });
--- TADirectory/index.js
+++ TADirectory/index.js
@@ -9,8 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
-//session
-app.use(cookieParser()); // Add cookie parser middleware
+app.use(cookieParser()); 
 
 app.use("/TADirectory", Router);
 
