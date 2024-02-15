221cddbdacb31bc477b4b488cb06fd31079b1d9f
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2024-02-15 14:39:13 +0200

    fix schedules page

---
+++ client/js/groupSchedule.js
@@ -0,0 +1,18 @@
+document
+  .getElementById("tableSelect")
+  .addEventListener("change", function () {
+    var tableIds = ["1", "2", "3", "4", "5", "6", "7"];
+
+    for (let i = 0; i < tableIds.length; i++) {
+      var table = document.getElementById(tableIds[i]);
+      table.style.opacity = 0;
+
+      table.style.display = "none";
+    }
+    var selectedTable = document.getElementById(this.value);
+    selectedTable.style.display = "flex";
+
+    setTimeout(function () {
+      selectedTable.style.opacity = 1;
+    }, 50);
+  });
