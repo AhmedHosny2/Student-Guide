7539dedd2f0fb3d201ce30e227309c66e9a34392
MooTamer <mohamedtamer200200@gmail.com>
2024-02-25 11:07:57 +0200

    unify cards UI in key links

--- client/js/resource gateway.js
+++ client/js/resource gateway.js
@@ -8,32 +8,38 @@ const cardsData = {
   "CMS Downloader": [
     "https://github.com/AhmedAshrafAZ/cms-downloader",
     "Automate Your Downloads Effortlessly! Fetch, Create Folders, and Download All Your CMS Content with Ease!",
-    "cmsDownloader"
+    "cmsDownloader",
+    "Open",
   ],
   "ACM Club": [
     "https://www.facebook.com/profile.php?id=61556831184217",
     "Level Up Your Skills: Join ACM Club for Problem-Solving and Programming Mastery!",
-    "acmClub"
+    "acmClub",
+    "Join",
   ],
   "GIU Calendar": [
     "https://drive.google.com/file/d/1PwTV77dy07mzuBjtGLd32Bqzrz0DNygr/view?usp=sharing",
     "Unlock Your Potential: Academic Semester Calendar Unveiled!",
-    "giuCalendar"
+    "giuCalendar",
+    "Open",
   ],
   "13- Whatsapp Group": [
     "https://chat.whatsapp.com/CWoFNpYmuSYAbVULWKxCtT",
     "Join the GIU Study WhatsApp group for 13- students",
-    "whatsappGroup13"
+    "whatsappGroup13",
+    "Join",
   ],
   "10- Whatsapp Group": [
     "https://chat.whatsapp.com/BflwNdGVwgVLaohbIr7nvU",
     "Join the GIU Study WhatsApp group for 10- students",
-    "whatsappGroup10"
+    "whatsappGroup10",
+    "Join",
   ],
   "GIU WIR": [
     "https://drive.google.com/file/d/1WHAYGxa3jRV1gGBhok-6RGGdvwewy3ye/view?usp=sharing",
     "Do wanna have a discount using your student ID? Check out the WIR program!",
-    "giuWIR"
+    "giuWIR",
+    "Open",
   ],
 };
 
@@ -42,9 +48,24 @@ Object.entries(cardsData).forEach(([key, value]) => {
   pageData.style.display = "grid";
 
   const cardContainer = document.querySelector(".card-wrap");
+
   // create new card
   let card = document.createElement("div");
   card.classList.add("card");
+
+  // create card head
+  let cardHead = document.createElement("div");
+  cardHead.classList.add("card-head");
+
+  // create card title
+  let cardTitle = document.createElement("h4");
+  cardTitle.classList.add("card-title");
+  cardTitle.textContent = key;
+
+  // append cardHead to Card
+  card.appendChild(cardHead);
+  cardHead.appendChild(cardTitle); // Append cardTitle to cardHead
+
   // create and add img data
   let img = document.createElement("img");
   img.src = `../images/${value[2]}.svg`;
@@ -52,31 +73,32 @@ Object.entries(cardsData).forEach(([key, value]) => {
   img.alt = "image not available";
   card.appendChild(img);
 
-  //create text content wrapper
+  // create text content wrapper
   const textContent = document.createElement("div");
   textContent.classList.add("txt");
 
   // create and add card text
   let cardText = document.createElement("p");
   cardText.classList.add("card-text");
-
   cardText.textContent = value[1];
   textContent.appendChild(cardText);
 
-  //create card title
-  const cardTitle = document.createElement("p");
-  cardTitle.classList.add("card-title");
-  cardTitle.textContent = key;
+  // create contact button
+  let cardButton = document.createElement("a");
+  cardButton.classList.add("contactBtn");
+  cardButton.textContent = value[3];
+  cardButton.href = value[0];
+  cardButton.target = "_blank";
+  textContent.appendChild(cardButton); // Append cardButton to textContent
 
-  //append card content
-  textContent.appendChild(cardTitle);
+  // append text content to card
   card.appendChild(textContent);
 
-  //append card
+  // append card
   cardContainer.appendChild(card);
 
-  //create on click function to pass to another page with link
-  card.addEventListener("click", () => {
-    window.open(value[0], "_blank");
+  // create onclick function for the contact button
+  cardButton.addEventListener("click", (event) => {
+    event.stopPropagation(); // Prevent card click event from triggering
   });
 });
