63a7f3a30d5e509767f78a25db70e7c03cb77597
Ahmed Yehia <98207790+AhmedHosny2@users.noreply.github.com>
2024-02-27 15:05:42 +0200

    Update profilePage.js

--- client/js/profilePage.js
+++ client/js/profilePage.js
@@ -1,5 +1,9 @@
-localStorage.setItem("isVerified", true);
-
+//  localStorage.setItem("isVerified", true);
+// localStorage.setItem("semester", "6");
+const userNameLS = localStorage.getItem("userName");
+const emailLS = localStorage.getItem("userEmail");
+const isVerifiedLS = localStorage.getItem("isVerified");
+const semesterLS = localStorage.getItem("semester");
 const idCardElement = document.querySelector(".profile-wrap .container .idCard");
 
 const isVerified = localStorage.getItem('isVerified') === 'true';
@@ -7,4 +11,15 @@ if (isVerified) {
   idCardElement.classList.add("verified");
 } else {
   idCardElement.classList.remove("verified");
+  // once clicked ref to the email verification page
+  idCardElement.addEventListener("click", () => {
+    window.location.href = "../html/verfiyEmail.html";
+  });
 }
+
+const userName = document.getElementById("accName");
+const email = document.getElementById("accEmail");
+const semester = document.getElementById("accSem");
+semester.innerHTML = semesterLS;
+email.innerHTML = emailLS;
+userName.innerHTML = userNameLS;
