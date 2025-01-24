f5d8222c3388c5c6af56d57caf5bb9b50120b079
Ahmed Yehia <98207790+AhmedHosny2@users.noreply.github.com>
2025-01-24 16:24:28 +0100

    fix .env problem

---
+++ client/utils/env.js
@@ -0,0 +1,19 @@
+// for deployed version
+
+
+// export const coursesURL = `https://student-guide-course.ahmed-yehia.me/course`;
+// export const taURL = `https://ta.ahmed-yehia.me/TADirectory`;
+// export const userURL = "https://student-guide-users.ahmed-yehia.me/user";
+// export const clientLoginURL = "https://guide.ahmed-yehia.me/html/login.html";
+// export const clientURL = "https://guide.ahmed-yehia.me/";
+// export const getCourseURL = "https://guide.ahmed-yehia.me/html/getCourse.html";
+
+// for local version
+
+
+export const userURL = "http://localhost:5001/user";
+export const taURL = `http://localhost:5003/TADirectory`;
+export const coursesURL = `http://localhost:5002/course`;
+export const clientURL = "http://localhost:5501/client/";
+export const clientLoginURL = "http://localhost:5501/client/html/login.html";
+export const getCourseURL = "./getcourse.html";
